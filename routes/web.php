7f4d863b0fc0b893f6f471468c3ca67f2dd21bfc
ghadanfar3 <ghadanfarheusen8@gmail.com>
2026-03-18 11:24:18 +0300

    project - theme to viwes module

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ThemeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+Route::controller(ThemeController::class)->group(function (){
+    Route::get("/", 'index')->name('index') ;
+    Route::get("/category", 'category')->name('category') ;
+    Route::get("/contact", 'contact')->name('contact') ;
+    Route::get("/single-blog", 'singleBlog')->name('singleBlog') ;
+    Route::get("/login", 'login')->name('login') ;
+    Route::get("/register", 'register')->name('register') ;
 });
+
+
+
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,3 +34,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
